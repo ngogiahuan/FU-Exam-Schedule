5e4de0d329e577009d2268fc788ed3696d353139
Nhan Ralph <dnhnhan2003@gmail.com>
2023-10-01 15:49:57 +0700

    complete navbar (add logo and navbar title)

--- fuexamschedule/src/component/Navbar/Navbar.js
+++ fuexamschedule/src/component/Navbar/Navbar.js
@@ -6,6 +6,14 @@ export default function Navbar() {
     return (
         <div className="navbar-container">
             <div className="navbar-element">
+                <div className='navbar-logo'>
+                    <img src="../assets/images/logo/logo-noBG.png" alt="" />
+                </div>
+
+                <div className="navbar-title">
+                    Exam Schedules
+                </div>
+
                 <Login />
             </div>
         </div>
