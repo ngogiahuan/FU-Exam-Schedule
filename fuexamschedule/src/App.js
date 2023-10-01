d07ed40e46bbeb67895e1ae622eee5367e6780ee
Ngo Gia Huan <ngohuan122003@gmail.com>
2023-10-01 15:24:54 +0700

    add logo, session login, welcome component

--- fuexamschedule/src/App.js
+++ fuexamschedule/src/App.js
@@ -1,12 +1,24 @@
-
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import Welcome from './component/Welcome/Welcome';
+import Student from './component/Student/Student';
 
 
 function App() {
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
     <>
+
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+        {user ? (
+          <Route path='/student' element={<Student />} />
+        ) : (
+          <Route path='/' element={<Welcome />} />
+        )}
+      </Routes>
     </>
   );
 }
--- fuexamschedule/src/component/Navbar/Navbar.js
+++ fuexamschedule/src/component/Navbar/Navbar.js
@@ -3,8 +3,14 @@ import Login from '../Login/Login'
 import './Navbar.css'
 
 export default function Navbar() {
+    const userProfile = JSON.parse(localStorage.getItem('userProfile'));
     return (
         <div className="navbar-container">
+            <div className="navbar-element">
+                <img src="./logo/logo-no-text-nobg.png" alt="logo" className='navbar-logo' />
+                <img src="./logo/logo-text.png" alt="logo text" className='logo-text' />
+            </div>
+
             <div className="navbar-element">
                 <Login />
             </div>
