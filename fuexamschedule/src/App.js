8ab1d2ae01f1a732ac6ed694c36b0c0d38f5e2a5
Ngo Gia Huan <ngohuan122003@gmail.com>
2023-10-02 12:44:07 +0700

    fix bug deploy

--- fuexamschedule/src/App.js
+++ fuexamschedule/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './component/Navbar/Navbar';
 import Welcome from './component/Welcome/Welcome';
 import Student from './component/Student/Student';
-import CalendarTest from './component/TestComponent/CalendarTest';
 
 function App() {
   const userProfile = JSON.parse(sessionStorage.getItem('userProfile'));
--- fuexamschedule/src/index.js
+++ fuexamschedule/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import 'materialize-css/dist/css/materialize.min.css'
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
