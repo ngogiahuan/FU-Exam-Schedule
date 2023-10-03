3bf820858e4cdccc869c1876cb864a6722f69c1b
Nhan Ralph <dnhnhan2003@gmail.com>
2023-10-04 02:04:38 +0700

    edit Student Card to view the Exam slot

--- fuexamschedule/src/component/Student/Student.js
+++ fuexamschedule/src/component/Student/Student.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Student.css'
+import StuExamCard from '../StudentCard/StuExamCard'
 
 export default function Student() {
     return (
-        <div className='student'>Student</div>
+        <div className='student'>
+            <div className='sidebar'>
+                <h1 style={{ color: '#2866cd' , fontWeight: 'bold', textAlign: 'center'}}>FALL 23</h1>
+            </div>
+            <StuExamCard />
+        </div>
     )
 }
---
+++ fuexamschedule/src/component/StudentCard/StuExamCard.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+import './StuExamCard.css';
+
+import { ListOfStuExam } from './ListOfStuExam';
+
+const cardColors = ['#aee5ff', '#c6d7fb', '#ffb8d0', '#fee5e1', '#ffbfb3']; // Add more colors as needed
+
+export default function StuExamCard() {
+  if (ListOfStuExam.length === 0) {
+    return (
+      <div className="container" style={{ marginTop: '180px' }}  >
+        <Typography variant="h5" sx={{ fontWeight: 'bold', color: '#2866cd'}}>
+          You do not have any exams
+        </Typography>
+      </div>
+    )
+    
+  } else { 
+
+    return (
+      <div className="container">
+        {ListOfStuExam.map((stuExam, index) => (
+          <Card
+            key={index}
+            sx={{
+              width: '29%',
+              padding: 1,
+              borderRadius: 8,
+              background: cardColors[index % cardColors.length], // Use index to cycle through colors
+            }}
+          >
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                {stuExam.day}
+              </Typography>
+              <Typography variant="h4" component="div" sx={{ fontWeight: 'bold' }}>
+                {stuExam.subject}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.primary">
+                <table border="0" cellspacing="5">
+                  <tr>
+                    <td>Room</td>
+                    <td style={{ paddingLeft: 8 }}>{stuExam.room}</td>
+                  </tr>
+                  <tr>
+                    <td>Time</td>
+                    <td style={{ paddingLeft: 8 }}>{stuExam.time}</td>
+                  </tr>
+                  <tr>
+                    <td>Type</td>
+                    <td style={{ paddingLeft: 8 }}>{stuExam.type}</td>
+                  </tr>
+                  <tr style={{ fontWeight: 'bold'}}>
+                    <td>Note</td>
+                    <td style={{ paddingLeft: 8 }}>{stuExam.note}</td>
+                  </tr>
+                </table>
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+}
