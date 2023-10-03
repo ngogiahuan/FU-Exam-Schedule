269a5f662a9845274be6d2150a3a55aa07e3f4ed
Ngo Gia Huan <ngohuan122003@gmail.com>
2023-10-03 16:31:05 +0700

    edit welcome page

--- fuexamschedule/src/component/Welcome/Welcome.js
+++ fuexamschedule/src/component/Welcome/Welcome.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import './Welcome.css'
+import React from "react";
+import "./Welcome.css";
 
 export default function Welcome() {
     return (
         <div className="welcome-conatiner">
-            <div class="typing animate"></div>
-            <br />
-            <p class="typing2 animate"></p>
+            <div className="welcome-card">
+                <div className="card-element">
+                    <div className="welcome-line">
+                        <h1 className="animate-charcter">Welcome</h1>
+                        <h2>to <span className="animate-charcter">FU Exam Schedule</span></h2>
+                    </div>
+                    <div className="details-line">
+                        <p>
+                            To use this portal, you need to login with your Google account.
+                        </p>
+                        <p>Click on the login button on the top right corner to login.</p>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
