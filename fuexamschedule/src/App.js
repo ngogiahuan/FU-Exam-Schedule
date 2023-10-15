888781223018cbc73cc9d3aa76e42afebbed9129
Ngo Gia Huan <ngohuan122003@gmail.com>
2023-10-15 14:17:46 +0700

    edit app.js

--- fuexamschedule/src/App.js
+++ fuexamschedule/src/App.js
@@ -3,13 +3,8 @@ import "./App.css";
 import Navbar from "./component/Navbar/Navbar";
 import Welcome from "./component/Welcome/Welcome";
 import Student from "./component/Student/Student";
-import Footer from "./component/Footer/Footer";
 import Examiner from "./component/Examiner/Examiner";
 import Admin from "./component/Admin/Admin";
-import ManageStudent from "./component/Admin/ManageStudent";
-import ManageRoom from "./component/Admin/ManageRoom";
-import ManageExaminer from "./component/Admin/ManageExaminer";
-import ManageExam from "./component/Admin/ManageExam";
 
 function App() {
   const userProfile = JSON.parse(sessionStorage.getItem("userProfile"));
@@ -19,8 +14,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/student" element={<Student />} />
-        <Route path="/examiner" element={<Examiner />} />
+        <Route path="/student/:feature" element={<Student />} />
+        <Route path="/examiner/:feature" element={<Examiner />} />
         <Route path="/admin/:feature" element={<Admin />}></Route>
       </Routes>
     </div>
