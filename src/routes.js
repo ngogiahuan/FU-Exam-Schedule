d085d0444e23fe08509d2a171b46c2311db19f21
Nhan Ralph <dnhnhan2003@gmail.com>
2023-10-26 09:20:47 +0700

    add route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
 // import
-import React, { Component } from "react";
+import React from "react";
 import Dashboard from "views/Dashboard/Dashboard.js";
 import Tables from "views/Dashboard/Tables.js";
 import Calendar from "views/Dashboard/Calendar.js";
-import RTLPage from "views/RTL/RTLPage.js";
-import Profile from "views/Dashboard/Profile.js";
 import SignIn from "views/Pages/SignIn.js";
 import SignUp from "views/Pages/SignUp.js";
 
@@ -21,7 +19,7 @@ var dashRoutes = [
     path: "/dashboard",
     name: "Báo cáo",
     icon: <HomeIcon color="inherit" />,
-    // component: Dashboard,
+    component: Dashboard,
     layout: "/admin",
   },
   {
